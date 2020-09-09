f7c6840b9ed3b2c147109024ff94a9fe5e3f264b
yeqifeishi <yeqifeishi@live.com>
2020-09-09 21:06:28 +0800

    1

--- src/main/java/com/fy/wh/dao/SysDeptDao.java
+++ src/main/java/com/fy/wh/dao/SysDeptDao.java
@@ -1,4 +1,15 @@
 package com.fy.wh.dao;
 
+import com.fy.wh.entity.SysDept;
+
+import java.util.List;
+
 public interface SysDeptDao {
+    List<SysDept> queryALlDept();
+
+    List<SysDept> queryByName(String deptName);
+
+    void updateDept(SysDept sysDept);
+
+    void deleteDept(int deptId);
 }
